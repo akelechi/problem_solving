02ad15318fa2d201f5ed207e8400666a1085eefd
Kelechi Akwataghibe <akwatsblessed@gmail.com>
2024-01-29 23:58:01 -0500

    Almost there

--- camelCase.py
+++ camelCase.py
@@ -5,39 +5,35 @@ def camelCase(rawInput):
     second = rawInput[1]
     third = rawInput[2]
     if first == 'S':
-        print('Split incoming ...')
         indices = [idx for idx in range(len(third)) if third[idx].isupper()]
         third = third.lower()
         
         if indices[0]==0:
             combined_list = third[0]
         else:
-            combined_list = third[0:indices[0]-1]
+            combined_list = third[:indices[0]]
         
         for idx in range(len(indices)-1):
-            combined_list = "".join((combined_list, third[indices[idx]+1:indices[idx+1]-1], " ", third[indices[idx]]))
-            print("".join(('The upper case letter is ', third[indices[idx+1]])))
+            combined_list = "".join((combined_list, third[indices[idx]+1:indices[idx+1]], " "))
+        
         combined_list = "".join((combined_list, third[indices[-1]:]))
         if second=='M':
-            print('We have a method to split...')
             print(combined_list[:-2])
         else:
-            print('Definitely a Class or Variable...')
             print(combined_list)
     else:
-        print('Combine incoming...')
         theInput = third.split()
         pos = 0
         for word in theInput:
             word = word.lower()
             if pos > 0:
-                word[0] = word[0].upper()
+                beg = word[0].upper()
+                word = "".join((beg, word[1:]))
             pos = pos + 1
-            theInput[pos] = word
+            theInput[pos-1] = word
         combined_list = "".join(theInput)
         
         if second == 'M':
-            print('You need to result in a method')
             print("".join((combined_list, "()")))
         else:
             print(combined_list)
