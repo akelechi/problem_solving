beab9f63a0ce419d927be0b4d1f03b3ac9950dd9
Kelechi Akwataghibe <akwatsblessed@gmail.com>
2024-01-30 21:44:57 -0500

    More progress

--- camelCase.py
+++ camelCase.py
@@ -1,32 +1,25 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 def camelCase(rawInput):
-    combined_list = ""
     first = rawInput[0]
     second = rawInput[1]
     third = rawInput[2]
     if first == 'S':
-        indices = [idx for idx in range(len(third)) if third[idx].isupper()]
-        third = third.lower()
-        
-        if indices[0]==0:
-            combined_list = third[0]
+        if second == 'M':
+            output = getSplit(third[:-2])
+        elif second == 'C':
+            word = "".join((third[0].lower(), third[1:]))
+            output = getSplit(word)
         else:
-            combined_list = third[:indices[0]]
+            output = getSplit(third)
         
-        for idx in range(len(indices)-1):
-            combined_list = "".join((combined_list, third[indices[idx]+1:indices[idx+1]], " "))
+        print(output.strip())
         
-        combined_list = "".join((combined_list, third[indices[-1]:]))
-        if second=='M':
-            print(combined_list[:-2])
-        else:
-            print(combined_list)
     else:
         theInput = third.split()
         pos = 0
         for word in theInput:
             word = word.lower()
-            if pos > 0:
+            if (pos > 0) or (pos == 0 and second == 'C'):
                 beg = word[0].upper()
                 word = "".join((beg, word[1:]))
             pos = pos + 1
@@ -36,10 +29,26 @@ def camelCase(rawInput):
         if second == 'M':
             print("".join((combined_list, "()")))
         else:
-            print(combined_list)
+            print(combined_list.strip())
     
     
-
+def getSplit(camelWord):
+    combined_list = ""
+    indices = [idx for idx in range(len(camelWord)) if camelWord[idx].isupper()]
+    
+    if len(indices)>0:
+        for idx in range(len(indices)):
+            if idx == 0:
+                combined_list = camelWord[:indices[0]].lower()
+            elif idx < len(indices)-1:
+                combined_list = "".join((combined_list, " ", camelWord[indices[idx]:indices[idx+1]].lower()))
+            else:
+                combined_list = "".join((combined_list, " ", camelWord[indices[idx]:].lower()))
+    else:
+        combined_list = camelWord.lower()
+        
+    return combined_list.strip()
+        
 
 if __name__ == '__main__':
     rawInput = list(map(str, input().rstrip().split(';')))
